42faabd2cbc0614943050615e49c0769267d9b66
tanmay-devv <tanmaysharma3141@gmail.com>
2024-02-26 16:38:49 +0530

    build

---
+++ build/CMakeFiles/exegame.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exegame.dir/main.cpp.o"
+  "CMakeFiles/exegame.dir/main.cpp.o.d"
+  "exegame"
+  "exegame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exegame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
